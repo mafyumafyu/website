8cb8b81b4580ad5057ba0ab9d937a7e25180f3f0
Matti <70283346+Mattimated@users.noreply.github.com>
2023-08-10 03:59:18 -0400

    Customize admin panel

--- website/polls/models.py
+++ website/polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -9,6 +10,11 @@ class Question(models.Model):
     def __str__(self) -> str:
         return self.text
 
+    @admin.display(
+        boolean=True,
+        ordering="publish_date",
+        description="Published recently?"
+    )
     def was_published_recently(self) -> None:
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.publish_date <= now
--- website/website/settings.py
+++ website/website/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+import dotenv
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hh9z$q27_@aklvsav_%z^b%f@%!+m)a@e9*@%s+g=iadas8&&@'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
